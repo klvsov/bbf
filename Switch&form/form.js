0158ae199e3c2c53aec1e70a535b6c3d7483602a
klvsov <klvsov@gmail.com>
2019-03-22 14:01:37 +0200

    forms validation

--- Switch&form/form.js
+++ Switch&form/form.js
@@ -8,8 +8,6 @@ window.onload = function(){
 
     btn.addEventListener('click', stop, false);
 
-
-
     btn.addEventListener('click', function(){
       
         let form = document.querySelector('#modal_form');
@@ -22,8 +20,39 @@ window.onload = function(){
             form.style.display = 'none';
             overlay.style.display = 'none';
         })
-
     })
 
+    let btn_submit = document.querySelector('#modal_form input[type=submit]');
+    btn_submit.addEventListener('click', stop, false);
+    btn_submit.addEventListener('click', function(){
+        let form = document.forms[1];
+        let userName = document.forms[1].elements['userName'];
+        let email = document.forms[1].elements['email'];
+        let pay = document.forms[1].elements['pay'];
+        let isConfirm = document.getElementById('confirm');
+        
+        let warning = document.querySelector('.red');
 
+        if(!isConfirm.checked){
+            warning.textContent = "Потрібно підтвердити замовлення!!!";   
+        } else{
+            warning.textContent = "";
+        }
+
+        for(let i = 0; i < form.length; i++){
+            if(form[i].classList.contains('is-invalid')){
+                form[i].classList.remove('is-invalid');
+            } 
+        }
+
+        if(userName.value.length < 3){
+            userName.classList.add('is-invalid');
+            warning.textContent += " Уважно заповніть ім'я користувача!"
+        }
+
+        if(email.value.length < 3 || email.value.includes('@')){
+            email.classList.add('is-invalid');
+            warning.textContent += " Уважно заповніть едектронну пошту!"
+        }
+    })
 }
